357e2cb68a44e6109c4557c0e3f35ad4624014b1
Aditya D <aditya.dandotia@ens.enterprises>
2025-02-04 12:34:57 +0530

    mdy: sidebar

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -1,25 +1,25 @@
  <!-- Main Sidebar Container -->
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="{{ route('dashboard.index') }}" class="brand-link">
-      <img src="{{ asset('assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">TentHouse</span>
-    </a>
+   <!-- Brand Logo -->
+   <a href="{{ route('dashboard.index') }}" class="brand-link">
+     <img src="{{ asset('assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+     <span class="brand-text font-weight-light">TentHouse</span>
+   </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset('assets/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">{{ auth()->user()->name }}</a>
-        </div>
-      </div>
+   <!-- Sidebar -->
+   <div class="sidebar">
+     <!-- Sidebar user panel (optional) -->
+     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+       <div class="image">
+         <img src="{{ asset('assets/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+       </div>
+       <div class="info">
+         <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+       </div>
+     </div>
 
-      <!-- SidebarSearch Form -->
-      <!-- <div class="form-inline">
+     <!-- SidebarSearch Form -->
+     <!-- <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
           <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
           <div class="input-group-append">
@@ -30,140 +30,157 @@
         </div>
       </div> -->
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
+     <!-- Sidebar Menu -->
+     <nav class="mt-2">
+       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+         <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-               <!-- User start-->
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-layer-group"></i>
-              <p>
-                Users
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('admin.user.index')}}" class="nav-link">
-                  <i class="far fa-rectangle-list nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('admin.user.create')}}" class="nav-link">
-                  <i class="far fa-square-plus nav-icon"></i>
-                  <p>Add</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-           <!-- User end -->
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-rug"></i>
-              <p>
-                Products
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('admin.products.index')}}" class="nav-link">
-                  <i class="far fa-rectangle-list nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('admin.products.create')}}" class="nav-link">
-                  <i class="far fa-square-plus nav-icon"></i>
-                  <p>Add</p>
-                </a>
-              </li>
-            </ul>
-          </li>
+         <!-- User start-->
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+             <i class="nav-icon fa-solid fa-user-plus"></i>
+             <p>
+               Users
+               <i class="right fas fa-angle-left"></i>
+             </p>
+           </a>
+           <ul class="nav nav-treeview">
+             <li class="nav-item">
+               <a href="{{route('admin.user.index')}}" class="nav-link">
+                 <i class="far fa-rectangle-list nav-icon"></i>
+                 <p>List</p>
+               </a>
+             </li>
+             <li class="nav-item">
+               <a href="{{route('admin.user.create')}}" class="nav-link">
+                 <i class="far fa-square-plus nav-icon"></i>
+                 <p>Add</p>
+               </a>
+             </li>
+           </ul>
+         </li>
+         <!-- User end -->
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+            <i class="nav-icon fa-solid fa-couch"></i>
+             <p>
+               Products
+               <i class="right fas fa-angle-left"></i>
+             </p>
+           </a>
+           <ul class="nav nav-treeview">
+             <li class="nav-item">
+               <a href="{{route('admin.products.index')}}" class="nav-link">
+                 <i class="far fa-rectangle-list nav-icon"></i>
+                 <p>List</p>
+               </a>
+             </li>
+             <li class="nav-item">
+               <a href="{{route('admin.products.create')}}" class="nav-link">
+                 <i class="far fa-square-plus nav-icon"></i>
+                 <p>Add</p>
+               </a>
+             </li>
+           </ul>
+         </li>
 
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-layer-group"></i>
-              <p>
-                Product Categories
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('admin.products.category.index')}}" class="nav-link">
-                  <i class="far fa-rectangle-list nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('admin.products.category.create')}}" class="nav-link">
-                  <i class="far fa-square-plus nav-icon"></i>
-                  <p>Add</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <!-- group start-->
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-layer-group"></i>
-              <p>
-                Groups
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('admin.products.groups.index')}}" class="nav-link">
-                  <i class="far fa-rectangle-list nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('admin.products.groups.create')}}" class="nav-link">
-                  <i class="far fa-square-plus nav-icon"></i>
-                  <p>Add</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-           <!-- group end -->
-             
-          <li class="nav-item">
-                <a href="{{route('admin.payment.index')}}" class="nav-link">
-                <i class="fa-solid fa-file-invoice-dollar nav-icon"></i>
-                  <p>Payment</p>
-                </a>
-          </li>
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+             <i class="nav-icon fas fa-layer-group"></i>
+             <p>
+               Product Categories
+               <i class="right fas fa-angle-left"></i>
+             </p>
+           </a>
+           <ul class="nav nav-treeview">
+             <li class="nav-item">
+               <a href="{{route('admin.products.category.index')}}" class="nav-link">
+                 <i class="far fa-rectangle-list nav-icon"></i>
+                 <p>List</p>
+               </a>
+             </li>
+             <li class="nav-item">
+               <a href="{{route('admin.products.category.create')}}" class="nav-link">
+                 <i class="far fa-square-plus nav-icon"></i>
+                 <p>Add</p>
+               </a>
+             </li>
+           </ul>
+         </li>
+         <!-- group start-->
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+             <i class="nav-icon fa-solid fa-tag"></i>
+             <p>
+               Groups
+               <i class="right fas fa-angle-left"></i>
+             </p>
+           </a>
+           <ul class="nav nav-treeview">
+             <li class="nav-item">
+               <a href="{{route('admin.products.groups.index')}}" class="nav-link">
+                 <i class="far fa-rectangle-list nav-icon"></i>
+                 <p>List</p>
+               </a>
+             </li>
+             <li class="nav-item">
+               <a href="{{route('admin.products.groups.create')}}" class="nav-link">
+                 <i class="far fa-square-plus nav-icon"></i>
+                 <p>Add</p>
+               </a>
+             </li>
+           </ul>
+         </li>
+         <!-- group end -->
 
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-rug"></i>
-              <p>
-                Orders
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('admin.orders.index')}}" class="nav-link">
-                  <i class="far fa-rectangle-list nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('admin.orders.create')}}" class="nav-link">
-                  <i class="far fa-square-plus nav-icon"></i>
-                  <p>Add</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <!-- <li class="nav-item">
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+             <i class="nav-icon fa-solid fa-credit-card"></i>
+             <p>
+               Payment Methods
+               <i class="right fas fa-angle-left"></i>
+             </p>
+           </a>
+           <ul class="nav nav-treeview">
+             <li class="nav-item">
+               <a href="{{route('admin.payment.index')}}" class="nav-link">
+                 <i class="far fa-rectangle-list nav-icon"></i>
+                 <p>List</p>
+               </a>
+             </li>
+             <li class="nav-item">
+               <a href="" class="nav-link">
+                 <i class="far fa-square-plus nav-icon"></i>
+                 <p>Add</p>
+               </a>
+             </li>
+           </ul>
+         </li>
+
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+             <i class="nav-icon fa-solid fa-folder"></i>
+             <p>
+               Orders
+               <i class="right fas fa-angle-left"></i>
+             </p>
+           </a>
+           <ul class="nav nav-treeview">
+             <li class="nav-item">
+               <a href="{{route('admin.orders.index')}}" class="nav-link">
+                 <i class="far fa-rectangle-list nav-icon"></i>
+                 <p>List</p>
+               </a>
+             </li>
+             <li class="nav-item">
+               <a href="{{route('admin.orders.create')}}" class="nav-link">
+                 <i class="far fa-square-plus nav-icon"></i>
+                 <p>Add</p>
+               </a>
+             </li>
+           </ul>
+         </li>
+         <!-- <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
@@ -171,9 +188,9 @@
               </p>
             </a>
           </li> -->
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+       </ul>
+     </nav>
+     <!-- /.sidebar-menu -->
+   </div>
+   <!-- /.sidebar -->
+ </aside>
